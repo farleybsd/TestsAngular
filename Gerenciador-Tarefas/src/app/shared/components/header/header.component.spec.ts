52ec0d0c44e8bdaae3f1b2fe42d7e47dd6ffcbf4
farleybsd <farley.t.i@hotmail.com>
2025-03-12 12:21:23 -0300

    teste(HeaderComponent): Meu Primeiro Test

--- Gerenciador-Tarefas/src/app/shared/components/header/header.component.spec.ts
+++ Gerenciador-Tarefas/src/app/shared/components/header/header.component.spec.ts
@@ -1,21 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+     TestBed.configureTestingModule({
       imports: [HeaderComponent],
-    }).compileComponents();
+    });
+
+    await TestBed.compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Deve Renderizar O Titulo Corretamente', () => {
+    const h1DebugElemnt = fixture.debugElement.query(By.css('h1'));
+    expect(h1DebugElemnt.nativeElement.textContent).toBe('Gerenciador De Tarefa');
   });
+  
 });
