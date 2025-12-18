b0658999f4d3dbe795ab870a640f14d7cc277fcb
farleybsd <farley.ti@hotmail.com>
2025-12-18 10:25:54 -0300

    feat(ListComponent): Implementa listagem de tarefas de forma fixa usando DaysiUi e TailWind

--- Gerenciador-Tarefas/src/app/pages/list/list.component.spec.ts
+++ Gerenciador-Tarefas/src/app/pages/list/list.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
+import { By } from '@angular/platform-browser';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -15,7 +16,20 @@ describe('ListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Deve Listar As Tarefas', () => {
+    
+    const todoSection = fixture.debugElement.query(By.css('[data-testid="todo-list"]'));
+    expect(todoSection).toBeTruthy();
+    
+    const todoItems =todoSection.queryAll(By.css('[data-testid="todo-list-item"]'));
+    expect(todoItems.length).toBe(3);
+
+
+    const CompletedtodoSection = fixture.debugElement.query(By.css('[data-testid="Completed-list"]'));
+    expect(CompletedtodoSection).toBeTruthy();
+    
+    const CompletedItems =CompletedtodoSection.queryAll(By.css('[data-testid="Completed-list-item"]'));
+    expect(CompletedItems.length).toBe(3);
+
   });
 });
