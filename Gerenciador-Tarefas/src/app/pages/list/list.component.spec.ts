f70fcf75c39561b8ebf347defd1b4115ab7e5955
farleybsd <farley.ti@hotmail.com>
2025-12-18 18:34:21 -0300

    test(ListComponent): Ultiliza TaskService no Teste

--- Gerenciador-Tarefas/src/app/pages/list/list.component.spec.ts
+++ Gerenciador-Tarefas/src/app/pages/list/list.component.spec.ts
@@ -1,35 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 import { By } from '@angular/platform-browser';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
+import { Observable, of } from 'rxjs';
+import { Task } from 'src/app/shared/interfaces/task.interface';
+
+class FakeTaskService implements TasksService {
+  getAll = jest.fn();
+}
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let taskService: TasksService;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       imports: [ListComponent],
-    }).compileComponents();
+      providers: [
+        {
+          provide: TasksService,
+          useClass: FakeTaskService,
+        },
+      ],
+    });
 
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await TestBed.compileComponents();
+
+    taskService = TestBed.inject(TasksService);
   });
 
   it('Deve Listar As Tarefas', () => {
     
-    const todoSection = fixture.debugElement.query(By.css('[data-testid="todo-list"]'));
+    (taskService.getAll as jest.Mock).mockReturnValue(of([
+      { title: 'Item 1', completed: false },
+      { title: 'Item 2', completed: false },
+      { title: 'Item 3', completed: false },
+      { title: 'Item 4', completed: true },
+      { title: 'Item 5', completed: true },
+      { title: 'Item 6', completed: true },
+    ]));
+
+    fixture = TestBed.createComponent(ListComponent);
+    fixture.detectChanges();
+
+    const todoSection = fixture.debugElement.query(
+      By.css('[data-testid="todo-list"]')
+    );
     expect(todoSection).toBeTruthy();
-    
-    const todoItems =todoSection.queryAll(By.css('[data-testid="todo-list-item"]'));
+
+    const todoItems = todoSection.queryAll(
+      By.css('[data-testid="todo-list-item"]')
+    );
     expect(todoItems.length).toBe(3);
 
-
-    const CompletedtodoSection = fixture.debugElement.query(By.css('[data-testid="Completed-list"]'));
+    const CompletedtodoSection = fixture.debugElement.query(
+      By.css('[data-testid="Completed-list"]')
+    );
     expect(CompletedtodoSection).toBeTruthy();
-    
-    const CompletedItems =CompletedtodoSection.queryAll(By.css('[data-testid="Completed-list-item"]'));
-    expect(CompletedItems.length).toBe(3);
 
+    const CompletedItems = CompletedtodoSection.queryAll(
+      By.css('[data-testid="Completed-list-item"]')
+    );
+    expect(CompletedItems.length).toBe(3);
   });
 });
