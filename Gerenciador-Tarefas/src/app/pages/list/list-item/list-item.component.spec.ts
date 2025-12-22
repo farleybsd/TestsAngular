ee0ce68bb06d18b1dd9e1833a3c7bebc41e8143c
farleybsd <farley.ti@hotmail.com>
2025-12-22 18:48:11 -0300

    Test(ListItemComponent) Criando Test

--- Gerenciador-Tarefas/src/app/pages/list/list-item/list-item.component.spec.ts
+++ Gerenciador-Tarefas/src/app/pages/list/list-item/list-item.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListItemComponent } from './list-item.component';
+import { Task } from 'src/app/shared/interfaces/task.interface';
+import { TestHelper } from 'src/testing/helpers/test-help';
 
 describe('ListItemComponent', () => {
-  let component: ListItemComponent;
   let fixture: ComponentFixture<ListItemComponent>;
+  let testHelper: TestHelper<ListItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -11,11 +13,24 @@ describe('ListItemComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHelper = new TestHelper(fixture);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Deve Renderizar O Titulo Da Tarefa', () => {
+
+    const fakeTask : Task = {
+      title: 'Nome da Tarefa',
+      completed: false,
+
+    }
+      
+    fixture.componentRef.setInput('task',fakeTask)
+    fixture.detectChanges();
+    
+    const taskTitleDebugEl = testHelper.QueryByTestId("list-item-task-title");
+    const text = testHelper.getTextContextByTestId("list-item-task-title");
+
+    expect(text).toBe(fakeTask.title);
+
   });
 });
---
+++ Gerenciador-Tarefas/src/testing/helpers/test-help.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+export class TestHelper<T> {
+  constructor(private fixture: ComponentFixture<T>) {}
+
+  QueryByTestId(testId: string) {
+    return this.fixture.debugElement.query(
+      By.css(`[data-testeid="${testId}"]`)
+    );
+  }
+
+  getTextContextByTestId(testId: string) {
+    return this.QueryByTestId(testId).nativeElement.textContent;
+  }
+  
+}
